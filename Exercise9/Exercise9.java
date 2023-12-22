5eb20fd0220541d8f250b33ea315d8210165d232
syedazfar313 <154445162+syedazfar313@users.noreply.github.com>
2023-12-22 06:37:09 -0800

    create Exercise9

---
+++ Exercise9/Exercise9.java
@@ -0,0 +1,13 @@
+// 9. Write a Java program to compute the specified expressions and print the output.
+// Test Data:
+// ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+// Expected Output
+// 2.138888888888889
+package Exercise9;
+
+public class Exercise9 {
+    public static void main(String[] args) {
+        System.out.println(((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)));
+    }
+    
+}
