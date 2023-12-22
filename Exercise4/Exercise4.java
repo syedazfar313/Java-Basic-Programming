1f4ae79992f6738eeed4efa79e3a85f549ab90eb
syedazfar313 <154445162+syedazfar313@users.noreply.github.com>
2023-12-22 01:49:48 -0800

    create Excercise4

---
+++ Exercise4/Exercise4.java
@@ -0,0 +1,17 @@
+// 4. Write a Java program to print the results of the following operations.
+// Test Data:
+// a. -5 + 8 * 6
+// b. (55+9) % 9
+// c. 20 + -3*5 / 8
+// d. 5 + 15 / 3 * 2 - 8 % 3
+package Exercise4;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+        System.out.println(-5 + 8 * 6);
+        System.out.println((55+9) % 9);
+        System.out.println(20 + -3*5 / 8);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+
+    }
+}
