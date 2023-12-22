b6b59efb7cc4a5ea5f597a84f6a9229747740b3b
syedazfar313 <154445162+syedazfar313@users.noreply.github.com>
2023-12-22 02:27:37 -0800

    create Exeercise8

---
+++ Exercise8/Exercise8.java
@@ -0,0 +1,20 @@
+// 8. Write a Java program to display the following pattern.
+// Sample Pattern :
+
+//    J    a   v     v  a                                                  
+//    J   a a   v   v  a a                                                 
+// J  J  aaaaa   V V  aaaaa                                                
+//  JJ  a     a   V  a     a
+package Exercise8;
+
+public class Exercise8 {
+    public static void main(String[] args) {
+        System.out.println("   j  a   v   v   a" );
+System.out.println("   j  a a  v  v  a a");
+System.out.println("j  j  aaaa v v  aaaaa" );
+System.out.println(" jj   a  a  v   a   a" );
+        
+    }
+    
+
+}
