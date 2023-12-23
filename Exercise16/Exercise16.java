08e207ab27addfaefa1eb5de777201e0cecd36b5
syedazfar313 <154445162+syedazfar313@users.noreply.github.com>
2023-12-23 01:02:53 -0800

    create Exercise 16

---
+++ Exercise16/Exercise16.java
@@ -0,0 +1,21 @@
+// 16. Write a Java program to print a face.
+// Expected Output
+
+//  +"""""+                                                 
+// [| o o |]                                                
+//  |  ^  |                                                 
+//  | '-' |                                                 
+//  +-----+
+package Exercise16;
+
+public class Exercise16 {
+    public static void main(String[] args) {
+
+        System.out.println(" +\"\"\"\"\"\"+");
+        System.out.println("[|o   o |]");
+        System.out.println(" |  ^   |");
+        System.out.println(" | '-'  |");
+        System.out.println(" +------+");
+    }
+
+}
