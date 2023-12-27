b7c1b682d8df711d536ffd35250525d42e74141a
syedazfar313 <154445162+syedazfar313@users.noreply.github.com>
2023-12-27 02:31:48 -0800

    create Exercise 28

---
+++ Exercise28/ResultListDemo.java
@@ -0,0 +1,19 @@
+package Exercise28;
+
+public class ResultListDemo {
+    public static void main(String[] args) {
+        int[] result = new int[6];
+        result[0] = 77;
+        result[1] = 70;
+        result[2] = 89;
+        result[3] = 87;
+        result[4] = 67;
+        result[5] = 87;
+        System.out.println("mark of the 1st Subject :" + result[0]);
+        System.out.println("Marks of the 2nd subject :" + result[1]);
+        System.out.println("mark of the 3rd Subject :" + result[2]);
+        System.out.println("Marks of the  4th :" + result[3]);
+        System.out.println("mark of the First 5th :" + result[4]);
+        System.out.println("Marks of the second 6th :" + result[5]);
+    }
+}
