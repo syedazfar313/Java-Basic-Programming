ed7035431d8dddc6078f01e649866b213042138e
syedazfar313 <154445162+syedazfar313@users.noreply.github.com>
2023-12-28 04:11:39 -0800

    create  Exercise31

---
+++ Exercise31/HalfPyramid.java
@@ -0,0 +1,18 @@
+package Exercise31;
+
+/**
+ * HalfPyramid
+ */
+public class HalfPyramid {
+
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
