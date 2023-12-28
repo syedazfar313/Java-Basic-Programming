4d512725b8119c1aa0b75e2e36f2a83a827d182e
syedazfar313 <154445162+syedazfar313@users.noreply.github.com>
2023-12-28 06:51:51 -0800

    create Exercise31

---
+++ Exercise31/Invertedhp.java
@@ -0,0 +1,16 @@
+package Exercise31;
+
+public class Invertedhp {
+    public static void main(String[] args) {
+        int n = 5;
+        // outer loop
+        for (int i = n; i >= 1; i--) {
+            // inner loop
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+}
